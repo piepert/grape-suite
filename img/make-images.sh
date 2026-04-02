86ab64bcba6b18f99bbc30e20add74916e9c2b8c
PieperT <tristanpieper080803@gmail.com>
2026-04-02 17:39:12 +0200

    add(examples): exam03

--- img/make-images.sh
+++ img/make-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 # working directory should be img/ folder
 
-# for the README.md only homework01, seminar-paper01, slides01, protocol01 and essay01 are needed
+# for the README.md only homework01, seminar-paper01, slides01, exam03, protocol01 and essay01 are needed
 
 # homework01, page 1
 typst c --root .. ../examples/homework01.typ  "./homework01-{0p}.png" --format png --pages 1
@@ -10,6 +10,9 @@ typst c --root .. ../examples/homework01.typ  "./homework01-{0p}-solutions.png" 
 
 # essay01, pages 1-3
 typst c --root .. ../examples/essay01.typ  "./essay01-{0p}.png" --format png --pages 1-3
+
+# exam03
+typst c --root .. ../examples/exam03.typ  "./exam03-{0p}.png" --format png --pages 1-2,5
 
 # protocol01, pages 1-3
 typst c --root .. ../examples/protocol01.typ  "./protocol01-{0p}.png" --format png --pages 1-3
